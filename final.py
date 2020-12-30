91c3c1cdfe3a1b7ac2f8a19398fdc22d6cffaf6c
DoVanDung98 <kspmvandung@gmail.com>
2020-12-30 17:04:10 +0700

    final

--- final.py
+++ final.py
@@ -1,6 +1,6 @@
 from semseg.test import process_img
 
-def init_model(size,model_path):
+def init_model_segment(size,model_path):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     data_loader = get_loader("cityscapes")
     loader = data_loader(
@@ -15,38 +15,9 @@ def init_model(size,model_path):
     model = get_model({"arch": "hardnet"}, n_classes)
     state = convert_state_dict(torch.load(model_path, map_location=device)["model_state"])
     model.load_state_dict(state)
-    model.eval()
-    model.to(device)
+    return model
 
-    return device, model, loader
-
-def test(size,model_path,input,output):
-    device, model, loader = init_model(size,model_path)
-    proc_size = eval(size)
-
-    if os.path.isfile(input):
-        img_raw, decoded = process_img(input, proc_size, device, model, loader)
-        blend = np.concatenate((img_raw, decoded), axis=1)
-        # print(img_raw)
-        # print(decoded)
-        # print(blend.shape)
-        out_path = os.path.join(output, os.path.basename(input))
-        cv2.imwrite("/content/test.png", decoded)
-        cv2.imwrite(out_path, blend)
-
-    elif os.path.isdir(input):
-        print("Process all image inside : {}".format(input))
-
-        for img_file in os.listdir(input):
-            _, ext = os.path.splitext(os.path.basename((img_file)))
-            if ext not in [".png", ".jpg"]:
-                continue
-            img_path = os.path.join(input, img_file)
-
-            img, decoded = process_img(img_path, proc_size, device, model, loader)
-            # print(img)
-            # print(decoded)
-            blend = np.concatenate((img, decoded), axis=1)
-            # print(blend)
-            out_path = os.path.join(output, os.path.basename(img_file))
-            cv2.imwrite(out_path, blend)
+def get_segment(input_path, size, device, model, loader):
+    img_raw, decoded = process_img(input_path, size, device, model, loader)
+    return decoded
+    
