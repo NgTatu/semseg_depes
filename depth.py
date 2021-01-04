000f8792f54f560c4fc9dfd2abfb430c66ec601e
Nguyễn Tài Tuấn (VNCDLL-CTÐTKSAI) <taituan.ng@gmail.com>
2021-01-04 16:00:52 +0700

    Add to device

--- depth.py
+++ depth.py
@@ -31,22 +31,25 @@ def init_model_depth():
     encoder_path = os.path.join("models", model_name, "encoder.pth")
     depth_decoder_path = os.path.join("models", model_name, "depth.pth")
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # LOADING PRETRAINED MODEL
     encoder = networks.ResnetEncoder(18, False)
     depth_decoder = networks.DepthDecoder(num_ch_enc=encoder.num_ch_enc, scales=range(4))
 
-    loaded_dict_enc = torch.load(encoder_path, map_location='cpu')
+    loaded_dict_enc = torch.load(encoder_path, map_location=device)
     filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if k in encoder.state_dict()}
     encoder.load_state_dict(filtered_dict_enc)
 
-    loaded_dict = torch.load(depth_decoder_path, map_location='cpu')
+    loaded_dict = torch.load(depth_decoder_path, map_location=device)
     depth_decoder.load_state_dict(loaded_dict)
+    encoder.to(device)
     encoder.eval()
+    depth_decoder.to(device)
     depth_decoder.eval()
     return loaded_dict_enc, encoder, depth_decoder
 
 
-def get_depth(image_path, max_depth, min_depth, scale, loaded_dict_enc, encoder, depth_decoder):
+def get_depth(image_path, max_depth, min_depth, scale, loaded_dict_enc, encoder, depth_decoder, device):
   input_image = pil.open(image_path).convert('RGB')
   original_width, original_height = input_image.size
 
@@ -54,9 +57,11 @@ def get_depth(image_path, max_depth, min_depth, scale, loaded_dict_enc, encoder,
   feed_width = loaded_dict_enc['width']
   input_image_resized = input_image.resize((feed_width, feed_height), pil.LANCZOS)
 
-  input_image_pytorch = transforms.ToTensor()(input_image_resized).unsqueeze(0) 
+  input_image_pytorch = transforms.ToTensor()(input_image_resized).unsqueeze(0)
+
+  input_image_device = input_image_pytorch.to(device)
   with torch.no_grad():
-    features = encoder(input_image_pytorch)
+    features = encoder(input_image_device)
     outputs = depth_decoder(features)
 
   disp = outputs[("disp", 0)]
